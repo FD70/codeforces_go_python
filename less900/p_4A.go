e9cdd885007e5c585de13c3b6364003a1399611b
FD70000000 <halfl.ez.ser@mail.ru>
2023-09-03 16:55:26 +0300

    go: done p_4A

--- less900/p_4A.go
+++ less900/p_4A.go
@@ -5,5 +5,15 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	var w int
+
+	fmt.Scanf("%d", &w)
+
+	if (w > 3) && (w%2) == 0 {
+		fmt.Print("YES")
+	} else {
+		fmt.Print("NO")
+	}
 }
+
+// https://codeforces.com/problemset/problem/4/A
